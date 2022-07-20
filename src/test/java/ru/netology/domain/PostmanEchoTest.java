2651730cdb1d4177c6630e313b1f3d1219559217
alex-pakin <aleksei_pakin@mail.ru>
2022-07-20 23:48:55 +0300

    successful build

--- src/test/java/ru/netology/domain/PostmanEchoTest.java
+++ src/test/java/ru/netology/domain/PostmanEchoTest.java
@@ -19,7 +19,24 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("https://en.wikipedia.org/wiki/NationalAeronauticsAndSpaceAdministration"));
+                .body("data", equalTo("https://en.wikipedia.org/wiki/NASA"));
+
+    }
+
+    @Test
+    void shouldTestPostInRussianLanguage() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Как тебе такое, Илон Маск?") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Как тебе такое, Илон Маск?"));
 
     }
 }
