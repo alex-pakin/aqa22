69d61c4c0c6bd53f22717a9294af4c374d872c99
alex-pakin <aleksei_pakin@mail.ru>
2022-07-20 23:35:31 +0300

    fail build

--- src/test/java/ru/netology/domain/PostmanEchoTest.java
+++ src/test/java/ru/netology/domain/PostmanEchoTest.java
@@ -19,7 +19,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("https://en.wikipedia.org/wiki/NASA"));
+                .body("data", equalTo("https://en.wikipedia.org/wiki/NationalAeronauticsAndSpaceAdministration"));
 
     }
 }
